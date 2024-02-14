6eb3fcc24389e0c82ed153ea5c1a045f2787c41f
yasirali4817 <156353785+yasirali4817@users.noreply.github.com>
2024-02-14 02:18:33 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigations from './src/navigation';
+
+export default function App() {
+  return (
+   <Navigations/>
+  );
+}
